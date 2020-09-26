73a26bdd8f03dd1c5e5ff9dc114f6c37b4ccb065
peppeconti <46853326+peppeconti@users.noreply.github.com>
2020-09-27 00:34:59 +0200

    animations added

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
-// calculates the actual width of the figure after loading and sets heigth equal to it
+
 
 window.addEventListener('load', () => {
+
+// calculates the actual width of the figure after loading and sets heigth equal to it
+
 let about_preview = document.querySelector('.preview_about figure');
 let actual_width = about_preview.getBoundingClientRect().width;
 // console.log(actual_width);
 about_preview.style.height = `${actual_width}px`;
+
+// add  incremental animation-delay to span element 
+
+let letters_container = document.querySelectorAll('.bounce');
+let letters_container_array = Array.from(letters_container);
+console.log(letters_container_array);
+letters_container_array.forEach((e) => {
+    let letters = e.children;
+    let letters_array = Array.from(letters);
+    let delay = 0;
+    letters_array.forEach((e) => {
+        e.style.animationDelay = `${delay}s`;
+        delay = delay + 0.05;
+    })
+});
+
 });
 
 // sets heigth equal to width on resize
@@ -14,24 +33,4 @@ window.addEventListener('resize', () => {
     let actual_width = about_preview.getBoundingClientRect().width;
     // console.log(actual_width);
     about_preview.style.height = `${actual_width}px`;
-});
-
-// add  incremental animation-delay to span
-
-let letters = document.querySelectorAll('button span');
-let letters_array = Array.from(letters);
-console.log(letters_array);
-let delay = 0;
-letters_array.forEach((e) => {
-    e.style.animationDelay = `${delay}s`;
-    delay = delay + 0.05;
-});
-
-let letters_2 = document.querySelectorAll('li span');
-let letters_array_2 = Array.from(letters_2);
-console.log(letters_array_2);
-let delay_2 = 0;
-letters_array_2.forEach((e) => {
-    e.style.animationDelay = `${delay_2}s`;
-    delay_2 = delay_2 + 0.05;
 });
