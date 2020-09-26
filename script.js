b42daace8d352ed7b11309bc1e632ec2e2b74acd
peppeconti <46853326+peppeconti@users.noreply.github.com>
2020-09-26 21:31:23 +0200

    add bouncing effect

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ window.addEventListener('resize', () => {
     // console.log(actual_width);
     about_preview.style.height = `${actual_width}px`;
 });
+
+// add  incremental animation-delay to span
+
+let letters = document.querySelectorAll('button span');
+let letters_array = Array.from(letters);
+console.log(letters_array);
+let delay = 0;
+letters_array.forEach((e) => {
+    e.style.animationDelay = `${delay}s`;
+    delay = delay + 0.05;
+});
+
+let letters_2 = document.querySelectorAll('li span');
+let letters_array_2 = Array.from(letters_2);
+console.log(letters_array_2);
+let delay_2 = 0;
+letters_array_2.forEach((e) => {
+    e.style.animationDelay = `${delay_2}s`;
+    delay_2 = delay_2 + 0.05;
+});
