88dbe26cedb0580efb940b8b2f1c693b53407ae1
peppeconti <46853326+peppeconti@users.noreply.github.com>
2020-10-05 19:16:23 +0200

    update
    Hamburger  menù e button spostati nel body

--- javascript/hamburger_menu.js
+++ javascript/hamburger_menu.js
@@ -2,41 +2,40 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict'; 
 
     // place each node of the 'ul' inside the grid relative to the row-structure  
-
+    const button = document.getElementById('button_menu');
     const container = document.getElementById('hamb_menu');
     let list_els = document.querySelector('#hamb_menu ul').children;
     let array_from_ul = Array.from(list_els);
-    console.log(array_from_ul);
+    // console.log(array_from_ul);
+    // console.log(button);
     let grid_start = 1;
     let grid_end = 2;
     array_from_ul.forEach(e => {
         e.style.gridRowStart = grid_start;
         e.style.gridRowEnd = grid_end;
-        e.style.backgroundColor = 'grey';
+        // e.style.backgroundColor = 'grey';
         grid_start++;
         grid_end++;
-        e.style.transform = `translateX(-110%) skewX(20deg)`;
-        e.style.opacity = '0.5';
+        //e.style.transform = `translateX(-110%) skewX(20deg)`;
+        //e.style.opacity = '0.5';
     })
 
-    document.addEventListener('keydown', (evt) => {
-        if (evt.code === 'KeyB') {
+    button.addEventListener('click', (evt) => {
+        if (container.style.zIndex !== '10') {
             container.style.zIndex = '10';
             let delay = 0.1;
             array_from_ul.forEach(e => {
                 e.style.transition = `${delay}s`;
                 delay = delay + 0.1;
-                e.style.transform = `translateX(0) skewX(0deg)`;
-                e.style.opacity = '1';
+                e.classList.add('goes_in')
             })
         }
-        if (evt.code ==='KeyC' ) {
+        else if (container.style.zIndex === '10') {
             let delay = 0.1;
             array_from_ul.forEach(e => {
                 e.style.transition = `${delay}s`;
                 delay = delay + 0.1;
-                e.style.transform = `translateX(-110%) skewX(20deg)`;
-                e.style.opacity = '0.5';
+                e.classList.remove('goes_in')
             })
             console.log(delay)
             setTimeout(function(){
